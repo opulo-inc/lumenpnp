831b9ab1ffa4ba26d05d5bafbe587bd9ada09135
Stephen Hawes <stephen@robot.local>
2020-10-15 13:19:24 -0400

    bare bones test firmware added

--- feeder/code/firmware_feeder/src/define.h
+++ feeder/code/firmware_feeder/src/define.h
@@ -1,6 +1,17 @@
-#define LED1 PB3
-#define LED2 PA2
+#define LED1 PB3 //green, on top
+#define LED2 PA2 //yellow, on bottom
 
 #define SW1 PA4
+#define SW2 PA5
+
+#define DRIVE1 PB0
+#define DRIVE2 PB1
+#define PEEL1 PA6
+#define PEEL2 PA7
+
+#define FILM_TENSION PA15
+#define TAPE_DETECT PA11
+#define SLOT_DETECT PA3
+#define OPTO_SIG PA0
 
 //#define DEBUG
--- feeder/code/firmware_feeder/src/main.cpp
+++ feeder/code/firmware_feeder/src/main.cpp
@@ -16,13 +16,14 @@ When the feeder receives a signal from the host, it indexes a certain number of 
 #include "define.h"
 #include <Arduino.h>
 
-
 //-----
 //timing variables
 //-----
 unsigned long startMillis;  
 unsigned long currentMillis;
 
+
+int i = 0;
 
 //-------
 //SETUP
@@ -37,19 +38,83 @@ void setup() {
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(SW1, INPUT_PULLUP);
+  pinMode(SW2, INPUT_PULLUP);
+
+  pinMode(TAPE_DETECT, INPUT_PULLUP);
+  pinMode(FILM_TENSION, INPUT_PULLUP);
+
+  pinMode(DRIVE1, OUTPUT);
+  pinMode(DRIVE2, OUTPUT);
+  pinMode(PEEL1, OUTPUT);
+  pinMode(PEEL2, OUTPUT);
+
+  //init led blink
+  for(int i = 0;i <= 5;i++){
+    if(i%2 == 0){
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      delay(200);
+    }
+    else{
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      delay(200);
+    }
+  }
+
+//setting boot pin states
+digitalWrite(LED1, HIGH);
+digitalWrite(LED2, HIGH);
+
 
 }
+
+
 
 //------
 //MAIN CONTROL LOOP
 //------
 void loop() {
-  digitalWrite(LED1, HIGH);
-  digitalWrite(LED2, LOW);
 
-  delay(500);
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, HIGH);
-  delay(500);
+
+  if(!digitalRead(FILM_TENSION)){//if film tension switch not clicked
+    //then spin motor to wind film
+    digitalWrite(PEEL2, LOW);
+    analogWrite(PEEL1, 200);
+    delay(20);
+  }
+  else{
+    digitalWrite(PEEL2, 0);
+    analogWrite(PEEL1, 0);
+  }
+
+
+
+
+
+  if(!digitalRead(SW1)){
+    analogWrite(DRIVE1, 200);
+    analogWrite(DRIVE2, 0);
+    digitalWrite(LED1, LOW);
+  }
+  else{
+    analogWrite(DRIVE1, 0);
+    analogWrite(DRIVE2, 0);
+    digitalWrite(LED1, HIGH);
+  }
+
+  if(!digitalRead(SW2)){
+    analogWrite(DRIVE2, 200);
+    analogWrite(DRIVE1, 0);
+    digitalWrite(LED2, LOW);
+  }
+  else{
+    analogWrite(DRIVE2, 0);
+    analogWrite(DRIVE1, 0);
+    digitalWrite(LED2, HIGH);
+  }
+
+  
+
 
 }
