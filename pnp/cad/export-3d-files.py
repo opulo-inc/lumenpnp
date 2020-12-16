49f87a2003e9ca14b6d1b2cc4a529bc108c9703f
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2020-12-16 11:52:01 +0000

    Script to generate 3D prints

---
+++ pnp/cad/export-3d-files.py
@@ -0,0 +1,20 @@
+FREECADPATH = '/usr/lib64/freecad/lib64/'
+import sys
+sys.path.append(FREECADPATH)
+import FreeCAD
+import MeshPart
+
+# Open Assembly
+assembly = FreeCAD.open("assembly.FCStd")
+
+for obj in assembly.Objects:
+    if ("b_FDM" in obj.Name):
+        print(obj.Name)
+        shape = obj.Shape.copy(False)
+        shape.Placement = obj.getGlobalPlacement()
+        mesh = assembly.addObject("Mesh::Feature", "Mesh")
+        mesh.Mesh=MeshPart.meshFromShape(Shape=shape, LinearDeflection=0.01, AngularDeflection=0.0872665, Relative=False)
+        mesh.Label=obj.Name
+        mesh.Mesh.write("3D-Prints/" + obj.Name + ".obj")
+
+FreeCAD.closeDocument(assembly.Name)
