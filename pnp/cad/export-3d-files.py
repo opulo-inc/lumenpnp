f52f56cafc68fe72e3ab66ad1e721c48d3130f6a
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2020-12-19 11:01:41 +0000

    Filter duplicates and simplify name

--- pnp/cad/export-3d-files.py
+++ pnp/cad/export-3d-files.py
@@ -8,13 +8,12 @@ import MeshPart
 assembly = FreeCAD.open("assembly.FCStd")
 
 for obj in assembly.Objects:
-    if ("b_FDM" in obj.Name):
-        print(obj.Name)
-        shape = obj.Shape.copy(False)
+    if ("b_FDM" in obj.Name and not "Body_001_" in obj.Name):
+        shape = obj.Shape
         shape.Placement = obj.getGlobalPlacement()
         mesh = assembly.addObject("Mesh::Feature", "Mesh")
-        mesh.Mesh=MeshPart.meshFromShape(Shape=shape, LinearDeflection=0.01, AngularDeflection=0.0872665, Relative=False)
+        mesh.Mesh=MeshPart.meshFromShape(Shape=shape, LinearDeflection=0.01, AngularDeflection=0.025, Relative=False)
         mesh.Label=obj.Name
-        mesh.Mesh.write("3D-Prints/" + obj.Name + ".stl")
+        mesh.Mesh.write("3D-Prints/" + obj.Name.split("00_")[1].split("_001_")[0] + ".stl")
 
 FreeCAD.closeDocument(assembly.Name)
