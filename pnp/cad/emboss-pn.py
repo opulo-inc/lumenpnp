b8575c69059382b3e1cae94ddb53dc0865050f22
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2020-12-30 13:30:12 +0000

    Include FDM- as part of the pn

--- pnp/cad/emboss-pn.py
+++ pnp/cad/emboss-pn.py
@@ -14,7 +14,7 @@ for subdir, dirs, files in os.walk("FDM/"):
     for file in files:
         fdm_file = FreeCAD.open("./FDM/" + file.split('.')[0] + "/" + file)
         if (fdm_file.ShapeString.Label == "PN"):
-            fdm_file.ShapeString.String = file.split("FDM-")[1].split("_")[0] # get version number from file name
+            fdm_file.ShapeString.String = file.split("_")[0] # get version number from file name
             fdm_file.recompute()
 
 FreeCAD.closeDocument(fdm_file.Name)
