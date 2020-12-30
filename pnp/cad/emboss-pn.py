0e92e6d078d3969c748552b2a3bb5e7505865d74
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2020-12-30 11:59:49 +0000

    Python script to add PN to a ShapeString on FreeCAD

---
+++ pnp/cad/emboss-pn.py
@@ -0,0 +1,20 @@
+#
+# Sets version number to a ShapeString called PN
+#
+
+FREECADPATH = '/usr/lib64/freecad/lib64'
+
+import sys
+sys.path.append(FREECADPATH)
+import os
+import FreeCAD
+import Draft
+
+for subdir, dirs, files in os.walk("FDM/"):
+    for file in files:
+        fdm_file = FreeCAD.open("./FDM/" + file.split('.')[0] + "/" + file)
+        if (fdm_file.ShapeString.Label == "PN"):
+            fdm_file.ShapeString.String = file.split("FDM-")[1].split("_")[0] # get version number from file name
+            fdm_file.recompute()
+
+FreeCAD.closeDocument(fdm_file.Name)
