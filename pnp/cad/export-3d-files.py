66cfdb37517952343936dc3d1eaf900db85dfac1
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2021-01-28 08:33:15 +0000

    Change GitHub Action name

--- pnp/cad/export-3d-files.py
+++ pnp/cad/export-3d-files.py
@@ -17,6 +17,6 @@ for obj in assembly.Objects:
         mesh = assembly.addObject("Mesh::Feature", "Mesh")
         mesh.Mesh=MeshPart.meshFromShape(Shape=shape, LinearDeflection=0.01, AngularDeflection=0.025, Relative=False)
         mesh.Label=obj.Name
-        mesh.Mesh.write("3D-Prints/" + obj.Name.split("b_")[1] + ".stl")
+        mesh.Mesh.write("3D-Prints/" + obj.Name.split("b_")[1].split("_001_")[0] + ".stl")
 
 FreeCAD.closeDocument(assembly.Name)
