ff13d16afe3c50b2b8cf6e4b1b5c45b05bb2e21c
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2021-03-20 20:44:03 +0000

    Print Name, Label and type to debug STL gen script

--- pnp/cad/export-3d-files.py
+++ pnp/cad/export-3d-files.py
@@ -8,25 +8,27 @@ import FreeCAD
 import MeshPart
 
 for cad_file in os.listdir("FDM"):
-    print("Processing " + cad_file)
+    if(cad_file == "FDM-0014_down-camera-mount.FCStd"):
+        print("Processing " + cad_file)
 
-    doc = FreeCAD.open('FDM/'+cad_file)
+        doc = FreeCAD.open('FDM/'+cad_file)
 
-    # Getting file name from part number emboss
-    name = ""
-    for obj in doc.Objects:
-        if obj.isDerivedFrom("Part::Part2DObject"):
-            if (obj.Label == "PN"):
-                name = obj.String
+        # Getting file name from part number emboss
+        name = ""
+        for obj in doc.Objects:
+            print("Name: "+obj.Name" Label: "+obj.Label+" Type: "+obj.Type)
+            if obj.isDerivedFrom("Part::Part2DObject"):
+                if (obj.Label == "PN"):
+                    name = obj.String
 
-    # If there is no part number embossed throw error
-    if name == "":
-        raise ValueError("Part " + cad_file + " doesn't have a ShapeString called PN for part number emboss")
+        # If there is no part number embossed throw error
+        if name == "":
+            raise ValueError("Part " + cad_file + " doesn't have a ShapeString called PN for part number emboss")
 
-    # Generate STL
-    shape = doc.getObject("Body").Shape.copy(False)
-    shape.Placement = doc.Body.getGlobalPlacement()
-    mesh = doc.addObject("Mesh::Feature", "Mesh")
-    mesh.Mesh = MeshPart.meshFromShape(Shape=shape, LinearDeflection=0.01, AngularDeflection=0.025, Relative=False)
-    mesh.Mesh.write("3D-Prints/" + name + ".stl")
-    FreeCAD.closeDocument(doc.Name)
+        # Generate STL
+        shape = doc.getObject("Body").Shape.copy(False)
+        shape.Placement = doc.Body.getGlobalPlacement()
+        mesh = doc.addObject("Mesh::Feature", "Mesh")
+        mesh.Mesh = MeshPart.meshFromShape(Shape=shape, LinearDeflection=0.01, AngularDeflection=0.025, Relative=False)
+        mesh.Mesh.write("3D-Prints/" + name + ".stl")
+        FreeCAD.closeDocument(doc.Name)
