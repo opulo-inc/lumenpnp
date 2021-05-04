cf2f8c5d74394dca12e30b371d60130d4a760e92
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2021-05-04 14:07:37 +0100

    Generate GCODE for analysis

---
+++ pnp/cad/slice-3d-files.py
@@ -0,0 +1,25 @@
+#
+# Exports 3D STL files to a static image for documentation
+#
+
+import glob
+import subprocess
+import os
+
+for name in glob.glob("./3D-Prints/*.stl"):
+	print (name)
+	
+	file=os.path.abspath(name)
+
+#	f = open("/tmp/render_image.scad", "w")
+#	f.write("import(\"")
+#	f.write(file)
+#	f.write("\", convexity=3);")
+#	f.close()
+	
+	base = os.path.splitext(file)[0]
+	print(base)
+	subprocess.call(["./PrusaSlicer.AppImage","--load","PrusaSlicerConfig.ini","--info","--output",base+"_{used_filament}_{extruded_volume}_{print_time}.gcode", "-g", base ])
+	
+#if os.path.exists("/tmp/render_image.scad"):
+#    os.remove("/tmp/render_image.scad")
