3fb7c14758e54bf16345630a647d682362477aba
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2021-11-05 12:36:24 +0000

    Fix font paths and re-compute 3d models automatically when building STL files

--- pnp/cad/export-3d-files.py
+++ pnp/cad/export-3d-files.py
@@ -5,6 +5,7 @@ import sys
 import traceback
 from pathlib import Path
 from typing import List
+import time
 
 freecad_paths = [
     '/home/runner/work/index/index/squashfs-root/usr/lib',  # For CI when using AppImage
@@ -15,6 +16,9 @@ freecad_paths = [
     'c:/Program Files/FreeCAD 0.18/bin/',  # For Windows
     'c:/Program Files/FreeCAD 0.19/bin/',  # For Windows
 ]
+
+# Font file relative to this python script
+font_folder = '../../lib/fonts'
 
 for path in freecad_paths:
     if os.path.exists(path):
@@ -34,7 +38,7 @@ print(FreeCAD.Version())
 def get_shape_placement(print_plane):
     map_mode = print_plane.MapMode
     attachment = print_plane.AttachmentOffset
-    print(print_plane.MapMode, attachment)
+    #print(print_plane.MapMode, attachment)
 
     if map_mode == "ObjectXY":
         z_down = FreeCAD.Vector(0, 0, -1)
@@ -54,7 +58,7 @@ def get_shape_placement(print_plane):
 def process_file(cad_file: Path):
     print("Processing " + cad_file.name)
 
-    doc = FreeCAD.open(str(cad_file.absolute()))
+    doc = FreeCAD.open(str(cad_file.absolute()))  
 
     # Getting file name from part number emboss
     name_options = [obj.String for obj in doc.Objects if
@@ -66,9 +70,8 @@ def process_file(cad_file: Path):
         raise ValueError("Part " + cad_file.name + " doesn't have a ShapeString called PN for part number emboss")
 
     if cad_file.name[:8] != name[:8]:
-        # STL model filename does not match the part number embedded in the file
+        # STL model file name does not match the part number embedded in the file
         raise ValueError("Part " + cad_file.name[:8] + " doesn't match the part number in the FreeCad model - "+name[:8])
-
 
     body = [obj for obj in doc.Objects if obj.Label == "Body"]
 
@@ -77,10 +80,44 @@ def process_file(cad_file: Path):
         for obj in doc.Objects:
             print(f"- {obj.Label}")
 
-        raise Exception(f"Body not found in model {cad_file.name}")
+        raise Exception(f"Object named 'Body' not found in model {cad_file.name}")
 
     body = body[0]
 
+	# Find font references in the model and ensure they point to the correct font file
+    fonts = [obj for obj in doc.Objects if
+                    obj.isDerivedFrom("Part::Part2DObject") and hasattr(obj,"FontFile")]
+
+    for obj in fonts:
+        newfontfile=os.path.join(font_folder,os.path.split(obj.getPropertyByName("FontFile"))[1])
+        if os.path.isfile(newfontfile)==False:
+            raise FileNotFoundError(f"Cannot find font file {newfontfile}")
+
+        if newfontfile != obj.getPropertyByName("FontFile"):
+            print(f"\tCorrected '{obj.Label}' font file name from {obj.getPropertyByName('FontFile')}")
+            setattr(obj,"FontFile",newfontfile)
+            obj.touch()
+
+	
+	
+	# Recompute the model to ensure its valid and does not contain broken references or edges
+	# Mark each object as "changed"
+    for obj in doc.Objects:
+        obj.touch()
+
+	# Recompute the entire document
+    t0 = time.perf_counter()
+    doc.recompute(None,True,True)
+    t1 = time.perf_counter()
+    total = t1-t0
+    print(f"\tRecompute of model took {total:3f}s")
+
+    
+	# Now check for any invalid shapes
+    for obj in doc.Objects:
+        if 'Invalid' in obj.State:
+            raise Exception(f"Shape '{obj.Name}' in model '{cad_file.name}' is invalid")
+	
     shape = body.Shape.copy(False)
 
     print_planes = [obj for obj in doc.Objects if obj.Label == "PrintPlane"]
@@ -90,14 +127,16 @@ def process_file(cad_file: Path):
         if map_mode in ["ObjectXY", "ObjectXZ", "ObjectYZ"]:
             shape.Placement = get_shape_placement(plane)
         else:
-            print(f"Warning, cannot determine orientation of {cad_file.name} with map mode {map_mode}")
+            print(f"\tWarning, cannot determine orientation of {cad_file.name} with map mode {map_mode}")
+    else:
+	    print(f"\tWarning, missing PrintPlane object in file {cad_file.name}")
 
     # Generate STL
     mesh = doc.addObject("Mesh::Feature", "Mesh")
     mesh.Mesh = MeshPart.meshFromShape(Shape=shape, LinearDeflection=0.01, AngularDeflection=0.025, Relative=False)
     mesh.Mesh.write("3D-Prints/" + name + ".stl")
     FreeCAD.closeDocument(doc.Name)
-    print(f"Generated file 3D-Prints/{name}.stl")
+    print(f"\tGenerated file 3D-Prints/{name}.stl")
 
 
 if __name__ == '__main__':
@@ -108,13 +147,15 @@ if __name__ == '__main__':
     fdm_path = Path('FDM')
 
     exceptions: List[Exception] = []
-    for f in fdm_path.glob('*.FCStd'):
+    for f in sorted(fdm_path.glob('*.FCStd')):
         try:
             process_file(f)
         except Exception as ex:
-            print(f"An error occurred while processing {str(f)}: ")
-            traceback.print_exc()
-
+		    print(f"****")
+            print(f"\tAn error occurred while processing {str(f)}:")
+            print(f"\t{ex}")
+            #traceback.print_exc()
+		    print(f"****")
             exceptions.append(ex)
 
     if exceptions:
