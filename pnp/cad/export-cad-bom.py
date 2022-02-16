65f94cb74d041af1c4ab528fb6e72e84a5648af8
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2022-02-16 11:59:27 +0000

    Revised BOM build script and catalog

--- pnp/cad/export-cad-bom.py
+++ pnp/cad/export-cad-bom.py
@@ -11,7 +11,10 @@ import sys
 import traceback
 from pathlib import Path
 from pprint import pprint
-from typing import List, Match
+from typing import Dict, List, Match
+
+import csv
+from csv import DictReader
 
 freecad_paths = [
     '/home/runner/work/index/index/squashfs-root/usr/lib',  # For CI when using AppImage
@@ -29,8 +32,10 @@ for path in freecad_paths:
         print(f"Added possible FreeCAD path: {path}")
         sys.path.append(path)
 
+# These must go after the sys.path.append code
 import FreeCAD
 import Spreadsheet
+
 
 print('Python version:')
 print(sys.version)
@@ -43,174 +48,274 @@ def process_parts_list(sheet):
     # Determine the number of rows in sheet (why is there no API function for this?)
     try:
         for row in range(1, 999):
-            dummyread=sheet.get("A"+str(row))
+            dummyread = sheet.get("A"+str(row))
     except ValueError as e:
-        total_rows=row
-
+        total_rows = row
 
     # Determine the number of columns in sheet (why is there no API function for this?)
     try:
         for col in range(0, 999):
-            dummyread=sheet.get(str(chr(ord('A')+col)+"1"))
+            dummyread = sheet.get(str(chr(ord('A')+col)+"1"))
     except ValueError as e:
-        total_columns=col
+        total_columns = col
 
-    print(f"columns={total_columns} rows={total_rows}")
+    #print(f"columns={total_columns} rows={total_rows}")
 
     # Extract the column positions for the fields we are interested in
     for c in range(ord('A'), ord('A')+total_columns):
-        col_heading=sheet.get(str(chr(c)+'1'))
-        
-        if col_heading=="QTY":
-            col_quantity=c
-        elif col_heading=="DESCRIPTION":
-            col_description=c
-        elif col_heading=="SUPPLIER":
-            col_supplier=c
-        elif col_heading=="(FILENAME)":
-            col_filename=c
-        elif col_heading=="POS":
-            col_pos=c
-        elif col_heading=="IDENTNO":
-            col_identno=c
-        elif col_heading=="SUPP.IDENTNO":
-            col_supp_identno=c
-        elif col_heading=="SUPP.DESCRIPTION":
-            col_supp_description=c
+        col_heading = sheet.get(str(chr(c)+'1'))
+        if col_heading == "QTY":
+            col_quantity = c
+        elif col_heading == "DESCRIPTION":
+            col_description = c
+        elif col_heading == "SUPPLIER":
+            col_supplier = c
+        elif col_heading == "(FILENAME)":
+            col_filename = c
+        elif col_heading == "POS":
+            col_pos = c
+        elif col_heading == "IDENTNO":
+            col_identno = c
+        elif col_heading == "SUPP.IDENTNO":
+            col_supp_identno = c
+        elif col_heading == "SUPP.DESCRIPTION":
+            col_supp_description = c
         else:
             print(f"Ignoring column={col_heading}")
 
     # Extract the data into list/dictionary
-    parts=[]
+    parts = []
     for r in range(2, total_rows):
-        description=sheet.get(str(chr(col_description)+str(r)))
-        if description=="*":
-            description=""
+        description = sheet.get(str(chr(col_description)+str(r)))
+        if description == "*":
+            description = ""
 
-        supplier=sheet.get(str(chr(col_supplier)+str(r)))
-        if supplier=="*":
-            supplier=""
+        supplier = sheet.get(str(chr(col_supplier)+str(r)))
+        if supplier == "*":
+            supplier = ""
 
-        parts.append( { 
-            "quantity":sheet.get(str(chr(col_quantity)+str(r))),
-            "description":description,
-            "supplier":supplier,
-            "filename":sheet.get(str(chr(col_filename)+str(r)))
-            } )
+        parts.append({
+            "quantity": sheet.get(str(chr(col_quantity)+str(r))),
+            # "description":description,
+            # "supplier":supplier,
+            "filename": sheet.get(str(chr(col_filename)+str(r)))
+        })
 
     return parts
-
 
 
 def process_fasteners_list(sheet):
     # Determine the number of rows in sheet (why is there no API function for this?)
     try:
         for row in range(1, 999):
-            dummyread=sheet.get("A"+str(row))
+            dummyread = sheet.get("A"+str(row))
     except ValueError as e:
-        total_rows=row
-
+        total_rows = row
 
     # Determine the number of columns in sheet (why is there no API function for this?)
     try:
         for col in range(0, 999):
-            dummyread=sheet.get(str(chr(ord('A')+col)+"1"))
+            dummyread = sheet.get(str(chr(ord('A')+col)+"1"))
     except ValueError as e:
-        total_columns=col
+        total_columns = col
 
-    print(f"columns={total_columns} rows={total_rows}")
+    #print(f"columns={total_columns} rows={total_rows}")
 
     # Extract the column positions for the fields we are interested in
     for c in range(ord('A'), ord('A')+total_columns):
-        col_heading=sheet.get(str(chr(c)+'1'))
-        
-        if col_heading=="Qty":
-            col_quantity=c
-        elif col_heading=="Type":
-            col_description=c
+        col_heading = sheet.get(str(chr(c)+'1'))
+
+        if col_heading == "Qty":
+            col_quantity = c
+        elif col_heading == "Type":
+            col_description = c
         else:
             print(f"Ignoring column={col_heading}")
-        
 
     # Extract the data into list/dictionary
-    parts=[]
+    parts = []
     for r in range(2, total_rows):
-        description=sheet.get(str(chr(col_description)+str(r)))
-        parts.append( { 
-            "quantity":sheet.get(str(chr(col_quantity)+str(r))),
-            "description":description,
-            "supplier":"",
-            "filename":""
-            } )
+        description = sheet.get(str(chr(col_description)+str(r)))
+        parts.append({
+            "quantity": sheet.get(str(chr(col_quantity)+str(r))),
+            "description": description,
+            # "supplier":"",
+            # "filename":""
+        })
 
     return parts
 
-def output_part_list(filename,part_list:List):
+
+def generate_bom(part_list: List, fasteners_list: List, catalog:Dict):
+    bom = []
+
+    # Include all "AlwaysInclude" flagged items from catalog
+    for key in catalog.keys():
+        item=catalog[key]
+        if 'AlwaysInclude' in item:
+            if item['AlwaysInclude']!='':
+                bom.append(item)
+
+    # Generate a list of dict's containing each item in the BOM
+    # we cross check the parts against the catalog and merge the data together
+    for p in part_list:
+        pn = p['filename'][:8]
+        c = {}
+        if pn in catalog:
+            c = catalog[pn]
+        else:
+            print("Missing item from catalog", pn)
+
+        c['Part Number'] = pn
+        c['Quantity'] = p['quantity']
+
+        if pn.startswith("FDM"):
+            c['3d Model Filename'] = pn+'.stl'
+            c['FreeCad File'] = p['filename']
+
+        bom.append(c)
+
+    # Repeat for the fasteners_list
+    for p in fasteners_list:
+        c = {}
+        
+        for k in catalog.keys():
+            row=catalog[k]
+            if row['Part Name']==str(p['description']):
+                c = row
+                break
+
+        c['Quantity'] = p['quantity']
+        # Supply part name if it wasn't found in the catalog
+        if 'Part Name' not in c: 
+            c['Part Name'] = p['description']
+            print("WARNING: Catalog missing entry for ",p)
+
+        bom.append(c)
+
+    return bom
+
+
+def output_bom(filename: str, bom: List):
     print(f"Output file={filename}")
     f = open(filename, "w+")
 
-    f.write(f"|Description|Supplier|Filename|Quantity|\r\n")
-    f.write(f"|-----------|--------|--------|--------|\r\n")
+    # Human readable list of column titles
+    column_title_list = ["Part Number", "Quantity", "Part Name", "Comment", "Optional", "3d Model Filename",
+                         "FreeCad Model Filename", "Supplier", "Supplier URL", "Alt. Supplier", "Alt. Supplier URL"]
+    for title in column_title_list:
+        f.write('|')
+        f.write(title)
+    f.write('|')
+    f.write('\n')
 
-    for p in part_list:
-        print(p)
-        f.write(f"|{p['description']}|{p['supplier']}|{p['filename']}|{p['quantity']}|\n")
+    # table markdown style
+    for title in column_title_list:
+        f.write('|')
+        f.write('-----------')
+    f.write('|')
+    f.write('\n')
+
+    # Order of keys to extract from rows in bom list
+    field_list = ["Part Number", "Quantity", "Part Name", "Comment","Optional", "3d Model Filename",
+                  "FreeCad File", "Source 1", "Source 1 URL", "Source 2", "Source 2 URL"]
+    for row in bom:
+        for key in field_list:
+            f.write('|')
+            if key in row:
+                value=str(row[key])
+                
+                #Detect URL's and format them in markdown style
+                if value.startswith("https://"):
+                    value="[Link]("+value+")"
+
+                f.write(value)
+
+        f.write("|\n")
 
     f.close()
 
+
 if __name__ == '__main__':
-    part_list=[]
+
+    catalog = {}
+
+    # Parse Parts Catalog into a nested dict
+    with open(Path('..', 'LumenPnP Part Catalog.csv'), newline='') as csvfile:
+        #csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        csvreader = DictReader(csvfile)
+        for row in csvreader:
+            pn = row['Part Number']
+
+            if pn in catalog:
+                print("WARNING: Duplicate part number in catalog ", row)
+            else:
+                catalog[pn] = row
 
     main_assembly = Path('.').joinpath('assembly.FCStd')
+    print("Opening FreeCad file", main_assembly)
 
     doc = FreeCAD.open(str(main_assembly.absolute()))
 
     # Ideally we would rerun the generation of the BOM and Fasteners using code
     # similar to below, but this doesn't appear to work directly from Python
-    #FreeCADGui.activateWorkbench("A2plusWorkbench")
-    #FreeCADGui.runCommand('a2p_CreatePartlist',0)
-    #FreeCADGui.activateWorkbench("FastenersWorkbench")
-    #FreeCADGui.runCommand('FSMakeBOM',0)
-    #App.getDocument('assembly').getObject('Fasteners_BOM').ViewObject.doubleClicked()
+    # FreeCADGui.activateWorkbench("A2plusWorkbench")
+    # FreeCADGui.runCommand('a2p_CreatePartlist',0)
+    # FreeCADGui.activateWorkbench("FastenersWorkbench")
+    # FreeCADGui.runCommand('FSMakeBOM',0)
+    # App.getDocument('assembly').getObject('Fasteners_BOM').ViewObject.doubleClicked()
 
     # Get the PARTSLIST
-    sheet=doc.getObject('_PARTSLIST_')
+    part_list = []
+    sheet = doc.getObject('_PARTSLIST_')
     if sheet.isDerivedFrom("Spreadsheet::Sheet"):
-        sheet.recompute()   
+        sheet.recompute()
         if sheet.isValid():
-            part_list.extend(process_parts_list(sheet))
-
+            part_list = process_parts_list(sheet)
 
     # Repeat for FASTENERS
-    fasteners=doc.getObject('Fasteners_BOM')
+    fasteners_list = []
+    fasteners = doc.getObject('Fasteners_BOM')
     if fasteners.isDerivedFrom("Spreadsheet::Sheet"):
-        fasteners.recompute()   
+        fasteners.recompute()
         if fasteners.isValid():
-            part_list.extend(process_fasteners_list(fasteners))
+            fasteners_list = (process_fasteners_list(fasteners))
 
-
-    FreeCAD.closeDocument(doc.Name)   
+    FreeCAD.closeDocument(doc.Name)
 
     # Sort by filename+description
-    sorted_part_list = sorted(part_list, key=lambda x: x["filename"]+x["description"] )
 
     # Check for missing files/bad paths
     for p in part_list:
-        filename=p['filename']
-        if filename!="":
+        filename = p['filename']
+        if filename != "":
             if filename.startswith('FDM-'):
-                path = Path("FDM",p['filename'])
+                path = Path("FDM", p['filename'])
             elif filename.startswith('OTS-'):
-                path = Path("OTS",p['filename'])
+                path = Path("OTS", p['filename'])
             elif filename.startswith('CSM-'):
-                path = Path("CSM",p['filename'])
+                path = Path("CSM", p['filename'])
             elif filename.startswith('PCB-'):
-                path = Path("PCB",p['filename'])
+                path = Path("PCB", p['filename'])
             else:
-                path = Path("FST",p['filename'])
-            if path.is_file()==False:
-                print("Missing",path)
+                path = Path("FST", p['filename'])
+            if path.is_file() == False:
+                print("Missing", path)
+
+    bom = generate_bom(part_list, fasteners_list, catalog)
+
+    # Spit out catalog entries which are not used as a warning
+    used_parts=[]
+    for p in bom:
+        used_parts.append(p['Part Number'])
+
+    unused_part_list = list(set(list(catalog.keys())).difference(used_parts))
+    print("Unused catalog parts",unused_part_list)
+
+    # Sort based on part number
+    sorted_bom = sorted(bom, key=lambda x: x["Part Number"])
 
     # Generate the MD file
-    output_part_list(Path('.').joinpath('index_bom.md'),sorted_part_list)
- 
+    output_bom(Path('.').joinpath('index_bom.md'), sorted_bom)
+
+
--- pnp/cad/index_bom.md
+++ pnp/cad/index_bom.md
@@ -1,72 +1,88 @@
-|Description|Supplier|Filename|Quantity|
-|-----------|--------|--------|--------|
-|||94000A330_18-8 STAINLESS STEEL CAP NUT.STEP|3|
-|||CSM-0001_staging-plate.FCStd|1|
-|DIN985 Nut M5|||7|
-|||FDM-0001_front-left-leg.FCStd|1|
-|||FDM-0002_front-right-leg.FCStd|1|
-|||FDM-0003_back-left-leg.FCStd|1|
-|||FDM-0004_back-right-leg.FCStd|1|
-|||FDM-0005_y-gantry.FCStd|2|
-|||FDM-0006_x-motor-mount.FCStd|1|
-|||FDM-0007_frame-umbilical-mount.FCStd|1|
-|||FDM-0008_y-gantry-support.FCStd|2|
-|||FDM-0009_roller-spacer.FCStd|6|
-|||FDM-0010_y-gantry-umbilical-mount.FCStd|1|
-|||FDM-0011_x-gantry-front.FCStd|1|
-|||FDM-0012_x-gantry-back.FCStd|1|
-|||FDM-0013_x-idler-mount.FCStd|1|
-|||FDM-0014_down-camera-mount.FCStd|1|
-|||FDM-0015_down-light-mount.FCStd|1|
-|||FDM-0017_z-gantry-left.FCStd|1|
-|||FDM-0019_up-light-mount.FCStd|1|
-|||FDM-0020_up-camera-mount.FCStd|1|
-|||FDM-0021_foot.FCStd|1|
-|||FDM-0022_umbilical-guide.FCStd|1|
-|||FDM-0023_peek-cable-guide.FCStd|6|
-|||FDM-0024_nozzle-holder.FCStd|1|
-|||FDM-0025_pump-mount.FCStd|1|
-|||FDM-0027_belt-clamp.FCStd|6|
-|||FDM-0037_belt-tension-arm.FCStd|3|
-|||FDM-0039_z-gantry-right.FCStd|1|
-|||FDM-0040_z-gantry-backplate-left.FCStd|1|
-|||FDM-0041_z-gantry-backplate-right.FCStd|1|
-|||FDM-0042_valve-mount.FCStd|1|
-|||FDM-0044_x-gantry-tension-bar.FCStd|2|
-|||FDM-0046_umbilical-swivel-base.FCStd|1|
-|||FDM-0047_umbilical-swivel-extension.FCStd|1|
-|||FDM-0055_datum-board-mount.FCStd|1|
-|||FDM-9001_squaring-bracket.FCStd|2|
-|||FST-0016_M5-tslot-nut.step|34|
-|ISO4032 Nut M3|||43|
-|ISO4032 Nut M5|||31|
-|ISO4762 Screw M2.5x20|||4|
-|ISO4762 Screw M2.5x8|||8|
-|ISO4762 Screw M3x16|||11|
-|ISO4762 Screw M3x30|||9|
-|ISO4762 Screw M5x10|||30|
-|ISO4762 Screw M5x25|||14|
-|ISO4762 Screw M5x40|||14|
-|ISO7380-1 Screw M3x10|||18|
-|ISO7380-1 Screw M3x14|||14|
-|ISO7380-1 Screw M3x8|||31|
-|ISO7380-1 Screw M5x8|||16|
-|||OTS-0001-00_CP40-nozzle-holder.FCStd|1|
-|||OTS-0002-00_usb-webcam.FCStd|2|
-|||OTS-0003-00_vacuum-pump.FCStd|1|
-|||OTS-0005-00_zip-tie.FCStd|6|
-|||OTS-0007-00_y-gantry-shroud.FCStd|1|
-|||OTS-0008-00_x-gantry-shroud.FCStd|1|
-|||OTS-0009_pneumatic-splitter-URY6M-4M.STEP|1|
-|||OTS-0010_20x20-v-slot-600mm.step|7|
-|||OTS-0011_v-slot-roller.step|10|
-|||OTS-0012_nema17-stepper.step|4|
-|||OTS-0013_nema11-hollow-shaft-stepper.stp|1|
-|||OTS-0014_linear-rail-100mm-LML9B.step|4|
-|||OTS-0015_20x20-corner-bracket.step|6|
-|||OTS-0016_GT2-20-tooth-idler.STEP|4|
-|||OTS-0017_GT2-20-tooth-pulley.STEP|4|
-|||OTS-0018_pneumatic-coupler-MS4M-M5.STEP|1|
-|||OTS-0019_limit-switch-PCBA.STEP|3|
-|||OTS-0020-00_24v_3-Way_Solenoid_Valve_0526T_www.fspump.cn.FCStd|1|
-|||PCB-0001-00_index_motherboard.step|1|
+|Part Number|Quantity|Part Name|Comment|Optional|3d Model Filename|FreeCad Model Filename|Supplier|Supplier URL|Alt. Supplier|Alt. Supplier URL|
+|-----------|-----------|-----------|-----------|-----------|-----------|-----------|-----------|-----------|-----------|-----------|
+|CSM-0001|1|Staging plate|3mm thick laser cut steel||||||||
+|FDM-0001|1|front-left-leg|||FDM-0001.stl|FDM-0001_front-left-leg.FCStd|||||
+|FDM-0002|1|front-right-leg|||FDM-0002.stl|FDM-0002_front-right-leg.FCStd|||||
+|FDM-0003|1|back-left-leg|||FDM-0003.stl|FDM-0003_back-left-leg.FCStd|||||
+|FDM-0004|1|back-right-leg|||FDM-0004.stl|FDM-0004_back-right-leg.FCStd|||||
+|FDM-0005|2|y-gantry|||FDM-0005.stl|FDM-0005_y-gantry.FCStd|||||
+|FDM-0006|1|x-motor-mount|||FDM-0006.stl|FDM-0006_x-motor-mount.FCStd|||||
+|FDM-0007|1|frame-umbilical-mount|||FDM-0007.stl|FDM-0007_frame-umbilical-mount.FCStd|||||
+|FDM-0008|2|y-gantry-support|||FDM-0008.stl|FDM-0008_y-gantry-support.FCStd|||||
+|FDM-0009|6|roller-spacer|||FDM-0009.stl|FDM-0009_roller-spacer.FCStd|||||
+|FDM-0010|1|y-gantry-umbilical-mount|||FDM-0010.stl|FDM-0010_y-gantry-umbilical-mount.FCStd|||||
+|FDM-0011|1|x-gantry-front|||FDM-0011.stl|FDM-0011_x-gantry-front.FCStd|||||
+|FDM-0012|1|x-gantry-back|||FDM-0012.stl|FDM-0012_x-gantry-back.FCStd|||||
+|FDM-0013|1|x-idler-mount|||FDM-0013.stl|FDM-0013_x-idler-mount.FCStd|||||
+|FDM-0014|1|down-camera-mount|||FDM-0014.stl|FDM-0014_down-camera-mount.FCStd|||||
+|FDM-0015|1|down-light-mount|Print in white PLA for diffusion||FDM-0015.stl|FDM-0015_down-light-mount.FCStd|||||
+|FDM-0017|1|z-gantry-left|||FDM-0017.stl|FDM-0017_z-gantry-left.FCStd|||||
+|FDM-0019|1|up-light-mount|Print in white PLA for diffusion||FDM-0019.stl|FDM-0019_up-light-mount.FCStd|||||
+|FDM-0020|1|up-camera-mount|||FDM-0020.stl|FDM-0020_up-camera-mount.FCStd|||||
+|FDM-0021|1|Build plate/camera foot/support|||FDM-0021.stl|FDM-0021_foot.FCStd|||||
+|FDM-0022|1|umbilical-guide|||FDM-0022.stl|FDM-0022_umbilical-guide.FCStd|||||
+|FDM-0023|6|peek-cable-guide|||FDM-0023.stl|FDM-0023_peek-cable-guide.FCStd|||||
+|FDM-0024|1|nozzle-holder|||FDM-0024.stl|FDM-0024_nozzle-holder.FCStd|||||
+|FDM-0025|1|pump-mount|||FDM-0025.stl|FDM-0025_pump-mount.FCStd|||||
+|FDM-0027|6|belt-clamp|||FDM-0027.stl|FDM-0027_belt-clamp.FCStd|||||
+|FDM-0037|3|belt-tension-arm|||FDM-0037.stl|FDM-0037_belt-tension-arm.FCStd|||||
+|FDM-0039|1|z-gantry-right|||FDM-0039.stl|FDM-0039_z-gantry-right.FCStd|||||
+|FDM-0040|1|z-gantry-backplate-left|||FDM-0040.stl|FDM-0040_z-gantry-backplate-left.FCStd|||||
+|FDM-0041|1|z-gantry-backplate-right|||FDM-0041.stl|FDM-0041_z-gantry-backplate-right.FCStd|||||
+|FDM-0042|1|vacuum_valve_mount|||FDM-0042.stl|FDM-0042_valve-mount.FCStd|||||
+|FDM-0044|2|x-gantry-tension-bar|||FDM-0044.stl|FDM-0044_x-gantry-tension-bar.FCStd|||||
+|FDM-0046|1|umbilical swivel base|||FDM-0046.stl|FDM-0046_umbilical-swivel-base.FCStd|||||
+|FDM-0047|1|umbilical swivel extension|||FDM-0047.stl|FDM-0047_umbilical-swivel-extension.FCStd|||||
+|FDM-0055|1|datum-board-mount|Mounting for OpenPNP calibration PCB||FDM-0055.stl|FDM-0055_datum-board-mount.FCStd|||||
+|FDM-9000||motherboard-mount|Alternative mounting instead of using metal standoffs|Y|||||||
+|FDM-9001|2|Squaring bracket|Used during assembly to ensure build plate is aligned and frame is square|Y|FDM-9001.stl|FDM-9001_squaring-bracket.FCStd|||||
+|FDM-9001|2|Squaring bracket|Used during assembly to ensure build plate is aligned and frame is square|Y|FDM-9001.stl|FDM-9001_squaring-bracket.FCStd|||||
+|FDM-9002||FDM-9002_parametric-strip-feeder|Strip feeder with variable widths/sizes|Y|||||||
+|FDM-9003||FDM-9003_12mm-strip-feeder||Y|||||||
+|FDM-9004||FDM-9004_16mm-strip-feeder||Y|||||||
+|FDM-9005||FDM-9005_component-tray||Y|||||||
+|FDM-9006||FDM-9006_24mm-strip-feeder||Y|||||||
+|FDM-9007||pcb_holder_rightangle|Printable PCB holder which can be bolted and/or magnet attached to build plate|Y|||||||
+|FDM-9008||cp40-nozzle-camera-mask|Shield for CP40 nozzle to aid camera detection|Y|||||||
+|FDM-9009||board-holder|Simple PCB board holder (multiple required to hold PCB)|Y|||||||
+|FST-0016|34|M5 T-Slot Nuts|||||Amazon|[Link](https://amzn.to/3b1d14V)|||
+|FST-0017|3|M3 Cap Nut|||||McMaster|[Link](https://www.mcmaster.com/94000A330/)|||
+|FST-0018|31|ISO4032 Nut M5|||||McMaster|[Link](https://www.mcmaster.com/90592a095)|||
+|FST-0020|30|ISO4762 Screw M5x10|||||McMaster|[Link](https://www.mcmaster.com/93070a121)|||
+|FST-0021|14|ISO4762 Screw M5x25|||||McMaster|[Link](https://www.mcmaster.com/90128A249/)|||
+|FST-0024|43|ISO4032 Nut M3|||||McMaster|[Link](https://www.mcmaster.com/90592A085/)|||
+|FST-0029|11|ISO4762 Screw M3x16|||||McMaster|[Link](https://www.mcmaster.com/91292A115/)|||
+|FST-0031|9|ISO4762 Screw M3x30|||||McMaster|[Link](https://www.mcmaster.com/91292A022/)|||
+|FST-0032|8|ISO4762 Screw M2.5x8|||||McMaster|[Link](https://www.mcmaster.com/91239A756/)|||
+|FST-0033|7|DIN985 Nut M5|||||||||
+|FST-0034|4|ISO4762 Screw M2.5x20|||||||||
+|FST-0035|14|ISO4762 Screw M5x40|||||||||
+|FST-0036|18|ISO7380-1 Screw M3x10|||||||||
+|FST-0037|14|ISO7380-1 Screw M3x14|||||||||
+|FST-0038|31|ISO7380-1 Screw M3x8|||||||||
+|FST-0039|16|ISO7380-1 Screw M5x8|||||||||
+|OTS-0001|1|CP40 Holder|||||RobotDigg|[Link](https://www.robotdigg.com/product/799/OpenPnP-CP40-Holder)|Alibaba|[Link](https://www.alibaba.com/product-detail/SMT-SPARE-PARTS-SAMSUNG-CP40-NOZZLE_60863912898.html?spm=a2700.galleryofferlist.normal_offer.d_title.795d3049d6w6hc)|
+|OTS-0002|2|ELP Camera (720p, 6mm lens)|||||||||
+|OTS-0003|1|Vacuum Pump|||||Amazon|[Link](https://www.amazon.com/gp/product/B0786BQYKM/ref=ppx_yo_dt_b_asin_title_o01_s01?ie=UTF8&psc=1)|||
+|OTS-0005|10|Zip tie|||||||||
+|OTS-0007||Y Gantry Braided Cable Sleeve Cable Protector|||||||||
+|OTS-0008||X Gantry Braided Cable Sleeve Cable Protector|||||||||
+|OTS-0009|1|Pneumatic Union Y Reducer|||||Automation Direct|[Link](https://www.automationdirect.com/adc/shopping/catalog/pneumatic_components/push-to-connect_union_pneumatic_fittings_(thermoplastic)/union_y_reducer/ury6m-4m)|||
+|OTS-0010|7|20mm x 20mm Aluminum V-Slot - 600mm|||||Amazon|[Link](https://www.amazon.com/gp/product/B085BMVZM7/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1)|||
+|OTS-0011|10|V-Slot Rollers|||||Amazon|[Link](https://amzn.to/2Y71mP9)|||
+|OTS-0012|4|NEMA 17 Stepper Motor|||||Amazon|[Link](https://www.amazon.com/STEPPERONLINE-Stepper-63-74oz-Connector-Extruder/dp/B07LCHHQ97/ref=sr_1_4?dchild=1&keywords=nema+17+stepper+motor&qid=1621417893&s=industrial&sr=1-4)|||
+|OTS-0013|1|NEMA 11 Hollow Shaft Stepper 11HY3406-9SK842|||||RobotDigg|[Link](https://www.robotdigg.com/product/798/NEMA11-hollow-shaft-stepper-for-Pick-and-Place-Machine)|||
+|OTS-0014|4|9mm LML9B Miniature Linear Rail (100mm length)|||||Amazon|[Link](https://amzn.to/2Y2p8vJ)|||
+|OTS-0015|6|2020 Corner Bracket (metal)|||||Amazon|[Link](https://www.amazon.com/Black-Corner-Connector-Aluminum-Extrusion/dp/B01GF2LG86/ref=sr_1_58?crid=2GQK0KJNHKAPK&dchild=1&keywords=2020+aluminum+extrusion+bracket&qid=1621416786&s=hi&sprefix=2020+bracket+alumin%2Ctools%2C157&sr=1-58)|McMaster|[Link](https://www.mcmaster.com/5537T498/)|
+|OTS-0016|4|GT2 Idler|||||Amazon|[Link](https://www.amazon.com/WINSINN-Aluminum-Timing-Pulley-Printer/dp/B07BPGYX3G/ref=psdc_16411441_t1_B078SD2XGL)|||
+|OTS-0017|4|GT2 Pulleys|||||Amazon|[Link](https://www.amazon.com/WINSINN-Aluminum-Synchronous-Timing-Printer/dp/B077GNZK3J/ref=psdc_16411441_t1_B07JJLZTLV?th=1)|||
+|OTS-0018|1|Push-To-Connect Pneumatic Fitting - MS4M-M5|||||Automation Direct|[Link](https://www.automationdirect.com/adc/shopping/catalog/pneumatic_components/push-to-connect_r-thread_pneumatic_fittings_(thermoplastic)/male_straight_(hex_body)/ms4m-m5)|||
+|OTS-0019|3|Makerbot Limit Switch|||||Amazon|[Link](https://www.amazon.com/D-FLIFE-Printer-Mechanical-Switches-Makerbot/dp/B089K5MX21)|||
+|OTS-0020|1|3-Way_Solenoid_Valve|||||||||
+|OTS-0021||1kg Spool of PLA|||||Amazon|[Link](https://amzn.to/3rUwGiQ)|||
+|OTS-0022||GT2 Timing Belt - 5 Meters|||||Amazon|[Link](https://www.amazon.com/Mercurry-Meters-timing-Rostock-GT2-6mm/dp/B071K8HYB4/ref=sr_1_3?crid=1HKUJHXOT3UPI&dchild=1&keywords=gt2+timing+belt&qid=1595949621&s=industrial&sprefix=gt2+timing+%2Cindustrial%2C144&sr=1-3)|||
+|OTS-0023||Pneumatic Tubing (4mm OD)|||||Automation Direct|[Link](https://www.automationdirect.com/adc/shopping/catalog/pneumatic_components/flexible_pneumatic_tubing_-a-_hoses/straight_polyurethane_(pur)_tubing/5-z-32_inch_(4_mm)/pu532blk100)|||
+|OTS-0024||Pneumatic Tubing (6mm OD)|||||Automation Direct|[Link](https://www.automationdirect.com/adc/shopping/catalog/pneumatic_components/flexible_pneumatic_tubing_-a-_hoses/straight_polyurethane_(pur)_tubing/6_mm/pu6mblk100)|||
+|OTS-0025||CP40 nozzle CP40LV|||||RobotDigg|[Link](https://www.robotdigg.com/product/800/OpenPnP-nozzle-CP40LV)|Alibaba|[Link](https://www.alibaba.com/product-detail/CP40-nozzle-FOR-SAMSUNG-SMT-machine_60494600926.html?spm=a2700.galleryofferlist.normal_offer.d_title.5c136d88lxMN2d)|
+|OTS-0026||Rubber Band|||||Amazon|[Link](https://www.amazon.com/Alliance-26649-Advantage-Contains-Approx/dp/B008X09P96)|||
+|PCB-0001|1|Motherboard|||||||||
