7c2fb26c00bfc0ea791ea90a64b48ea74816da84
Stephen Hawes <sphawes@gmail.com>
2022-06-13 14:21:46 -0400

    fixes old repo reference in exporting csm and bom CI scripts

--- .github/workflows/scripts/export-bom.py
+++ .github/workflows/scripts/export-bom.py
@@ -76,7 +76,7 @@ tr:hover {
 f.write("LumenPnP BOM " + sys.argv[1])
 
 #write the beginning of table, and row for header
-f.write("</h1><a href='https://www.github.com/index-machines/index/releases/' target='_blank' rel='noopener noreferrer'><h2>Download the Source Here</h2></a><a href='https://www.opulo.io/' target='_blank' rel='noopener noreferrer'><img src='https://cdn.shopify.com/s/files/1/0570/4256/7355/files/opulo-white-alpha_1200x1200.png?v=1644515300' id='logo'/></a><table>")
+f.write("</h1><a href='https://www.github.com/opulo-inc/lumenpnp/releases/' target='_blank' rel='noopener noreferrer'><h2>Download the Source Here</h2></a><a href='https://www.opulo.io/' target='_blank' rel='noopener noreferrer'><img src='https://cdn.shopify.com/s/files/1/0570/4256/7355/files/opulo-white-alpha_1200x1200.png?v=1644515300' id='logo'/></a><table>")
 
 with open('bom.csv') as bom:
     csv_reader = csv.reader(bom, delimiter=',')
--- .github/workflows/scripts/export-csm.py
+++ .github/workflows/scripts/export-csm.py
@@ -8,7 +8,7 @@ from typing import List
 import time
 
 freecad_paths = [
-    '/home/runner/work/index/index/squashfs-root/usr/lib',  # For CI when using AppImage
+    '/home/runner/work/lumenpnp/lumenpnp/squashfs-root/usr/lib',  # For CI when using AppImage
     '/usr/lib/freecad/lib/',  # For CI
     '/usr/lib/freecad-daily-python3/lib/',  # For Ubuntu
     '/usr/lib64/freecad/lib64/',  # For Fedora
